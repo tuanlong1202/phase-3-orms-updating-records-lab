995c1205aa64401c85b087cffcdfc33091d43118
tuanlong1202 <tuanlong1202@gmail.com>
2021-08-08 22:44:39 -0400

    Phase 3 ORMs updating records lab

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,79 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  attr_reader :id
+  def initialize(name,grade,id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def id
+    @id
+  end
+
+  def self.create_table
+    sql =  <<-SQL 
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      grade TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)     
+  end
+
+  def self.drop_table
+    sql =  <<-SQL 
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)     
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name,grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    new_student = self.new(row[1],row[2],row[0])  # self.new is the same as running Student.new
+    new_student  # return the newly created instance
+  end
+  
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 end
